db9fb44a2eeff16260baac581c8d1f9e98808080
plizano-jccm <plizano@jccm.es>
2020-10-11 21:07:22 +0200

    Attributes e index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-"// Sample JS File" 
+// Sample JS File
+
+const API_KEY = "{SECURE_API_KEY}";
+
+const makeAPICall = function(key) {
+    console.log("Making API Call.....");
+}
+
+makeAPICall(API_KEY);
